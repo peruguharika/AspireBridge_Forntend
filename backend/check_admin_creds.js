6e498d16a557c8d3f61e6dd87c1c8ddac2bc6e69
192210034 <maveenkallu@gmail.com>
2026-01-09 10:24:35 +0530

    Stable version

--- backend/check_admin_creds.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-
-console.log('--- ADMIN CREDENTIALS CHECK ---');
-console.log('ADMIN_EMAIL:', process.env.ADMIN_EMAIL);
-console.log('ADMIN_PASSWORD:', process.env.ADMIN_PASSWORD);
-console.log('-------------------------------');
--- backend/test_mongoose.js
+++
@@ -1,7 +0,0 @@
-console.log('Loading mongoose...');
-try {
-    const mongoose = require('mongoose');
-    console.log('Mongoose loaded.');
-} catch (e) {
-    console.error('Failed to load mongoose:', e);
-}
